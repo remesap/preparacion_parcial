e2dcb161f661b681d613d17c9eb771b71878c6e9
Stefan <remesap@unal.edu.co>
2017-03-23 22:28:23 -0700

    ahora sí

--- forward.cpp
+++ forward.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cstudio>
+#include <cstdio>
 #include <cmath>
 
 double f (double x);
@@ -7,6 +7,14 @@ double forward_deriv( double x, double h);
 
 int main(void){
 
+  double h, x;
+
+  x=3.7;
+  for (int ii=1; ii<=10; ii++){
+
+    h = std::pow(10.0, -ii);
+    std::printf( "%25.16e%25.16e%25.16e\n", h, forward_deriv(x,h), 2*std::cos(2*x));
+  }
   return 0;
   
 }
